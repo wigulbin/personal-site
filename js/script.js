85c40820c3cd551cad1e21154140fecd9fa8d970
Will Gulbin <wigulbin@gmail.com>
2017-06-03 12:43:01 -0400

    Background image now responsive

--- js/script.js
+++ js/script.js
@@ -1,8 +1,5 @@
 $(document).ready(function() {
 	$('#fullpage').fullpage();
 
-	$(window).resize(function(){
-		console.log(document.documentElement.clientWidth);
-		console.log(document.documentElement.innerWidth);
-	})
+	
 });
