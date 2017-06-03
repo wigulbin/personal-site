1ee9e65c19c88a741d49c570daf74693dd1f1431
Will Gulbin <wigulbin@gmail.com>
2017-06-03 12:06:11 -0400

    Restructure page, remove jsScroll plugin

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
 	$('#fullpage').fullpage();
+
+	$(window).resize(function(){
+		console.log(document.documentElement.clientWidth);
+		console.log(document.documentElement.innerWidth);
+	})
 });
