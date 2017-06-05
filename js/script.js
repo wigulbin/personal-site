797ba7169d8f25028a7bf83994fcbf56f61567eb
Will Gulbin <wigulbin@gmail.com>
2017-06-04 22:53:41 -0400

    Complete site

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
 $(document).ready(function() {
-	$('#fullpage').fullpage();
-
-	
+  $("#wor-exp").click(function(){
+    $('html, body').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+    }, 1000);
+  });
+  $("#per-exp").click(function(){
+    $('html, body').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+    }, 1000);
+  });
+  $("#lan-exp").click(function(){
+    $('html, body').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+    }, 1000);
+  });
+  $("#con").click(function(){
+    $('html, body').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+    }, 1000);
+  });
 });
