181b815b745c673b1beff05110dbb609b7d2ab59
Will Gulbin <wigulbin@gmail.com>
2017-08-27 18:56:36 -0400

    Major site update

--- js/script.js
+++ js/script.js
@@ -3,21 +3,42 @@ $(document).ready(function() {
   $("#wor-exp").click(function(){
     $('html, body').animate({
       scrollTop: $($.attr(this, 'href')).offset().top
-    }, 1000);
+    }, 750);
   });
   $("#per-exp").click(function(){
     $('html, body').animate({
       scrollTop: $($.attr(this, 'href')).offset().top
-    }, 1000);
+    }, 750);
   });
   $("#lan-exp").click(function(){
     $('html, body').animate({
       scrollTop: $($.attr(this, 'href')).offset().top
-    }, 1000);
+    }, 750);
   });
   $("#con").click(function(){
     $('html, body').animate({
       scrollTop: $($.attr(this, 'href')).offset().top
-    }, 1000);
+    }, 750);
   });
+  $('img[src$=".svg"]').each(function() {
+        var $img = jQuery(this);
+        var imgURL = $img.attr('src');
+        var attributes = $img.prop("attributes");
+
+        $.get(imgURL, function(data) {
+            // Get the SVG tag, ignore the rest
+            var $svg = jQuery(data).find('svg');
+
+            // Remove any invalid XML tags
+            $svg = $svg.removeAttr('xmlns:a');
+
+            // Loop through IMG attributes and apply on SVG
+            $.each(attributes, function() {
+                $svg.attr(this.name, this.value);
+            });
+
+            // Replace IMG with SVG
+            $img.replaceWith($svg);
+        }, 'xml');
+    });
 });
